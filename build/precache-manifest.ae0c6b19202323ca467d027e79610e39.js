e179bb410d3b6edd00118d70eae05f956aa29270
GordeySpb <gordey_89@mail.ru>
2019-03-25 12:51:39 +0300

    Правит gitignore

---
+++ build/precache-manifest.ae0c6b19202323ca467d027e79610e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2db298a309a6fb30081a",
+    "url": "/static/js/main.2db298a3.chunk.js"
+  },
+  {
+    "revision": "daa13876f799fed731f9",
+    "url": "/static/js/1.daa13876.chunk.js"
+  },
+  {
+    "revision": "2db298a309a6fb30081a",
+    "url": "/static/css/main.cbf3e538.chunk.css"
+  },
+  {
+    "revision": "daa13876f799fed731f9",
+    "url": "/static/css/1.3c6f8a5b.chunk.css"
+  },
+  {
+    "revision": "83fd2637c610f6f4ada5cc2fa47a24c1",
+    "url": "/index.html"
+  }
+];
