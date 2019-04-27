5c61a83918b899c8ef2d3fe710d6af1060983ac4
GordeySpb <gordey_89@mail.ru>
2019-04-27 21:05:31 +0300

    Updates

---
+++ precache-manifest.1260919d341fc512dc42b52fb9efafbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f70abe812d3601be07a",
+    "url": "/GordeySpb.github.io/Shop-Test/static/js/runtime~main.9f70abe8.js"
+  },
+  {
+    "revision": "8c39fc3e2a10f30c7e43",
+    "url": "/GordeySpb.github.io/Shop-Test/static/js/main.8c39fc3e.chunk.js"
+  },
+  {
+    "revision": "daa13876f799fed731f9",
+    "url": "/GordeySpb.github.io/Shop-Test/static/js/1.daa13876.chunk.js"
+  },
+  {
+    "revision": "8c39fc3e2a10f30c7e43",
+    "url": "/GordeySpb.github.io/Shop-Test/static/css/main.cbf3e538.chunk.css"
+  },
+  {
+    "revision": "daa13876f799fed731f9",
+    "url": "/GordeySpb.github.io/Shop-Test/static/css/1.3c6f8a5b.chunk.css"
+  },
+  {
+    "revision": "173b36923270929066652c0fa13c0a3b",
+    "url": "/GordeySpb.github.io/Shop-Test/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/GordeySpb.github.io/Shop-Test/precache-manifest.1260919d341fc512dc42b52fb9efafbd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/GordeySpb.github.io/Shop-Test/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
