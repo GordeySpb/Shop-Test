b1a5fc82bf91dc2b8b83b09a0cfa835f5ef8b754
GordeySpb <gordey_89@mail.ru>
2019-04-27 20:16:13 +0300

    правит редьюсер

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-import reducer from "../reducers";
+import reducer from "../reducers/index";
 
 import localStorage, { loadState } from "../middleware/local-storage";
 import {
